7dc0708cd81572b169f7ab1160e26607ce84b2ad
padcoding1 <padcoding@gmail.com>
2024-07-08 08:50:03 -0600

    updated committed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
-// src/App.jsx
+// App.jsx
 
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
+import Landing from './components/Landing/Landing';
+import Dashboard from './components/Dashboard/Dashboard';
 
 const App = () => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
   return (
     <>
       <NavBar user={user} />
-      <h1>Hello world!</h1>
+      <Routes>
+        { user ? (
+          <Route path="/" element={<Dashboard user={user} />} />
+        ) : (
+          <Route path="/" element={<Landing />} />
+        )}
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
